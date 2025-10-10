7c3ba7cf1ffc5ad47634ee419b18bb677765494d
itsLarss <lars.schlich21@gmail.com>
2025-10-10 20:25:59 +0200

    Version 0.0.1-0.41-ALPHA

--- src/main/java/dev/itsLarss/arcanewilds/enchantment/ModEnchantmentEffect.java
+++ src/main/java/dev/itsLarss/arcanewilds/enchantment/ModEnchantmentEffect.java
@@ -2,7 +2,6 @@ package dev.itsLarss.arcanewilds.enchantment;
 
 import com.mojang.serialization.MapCodec;
 import dev.itsLarss.arcanewilds.ArcaneWilds;
-import dev.itsLarss.arcanewilds.enchantment.custom.AreaMiningBlockBreakHandler;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.enchantment.effects.EnchantmentEntityEffect;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,8 +11,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModEnchantmentEffect {
     public static final DeferredRegister<MapCodec<? extends EnchantmentEntityEffect>> ENTITY_ENCHANTMENT_EFFECTS =
             DeferredRegister.create(Registries.ENCHANTMENT_ENTITY_EFFECT_TYPE, ArcaneWilds.MOD_ID);
-
-
 
     public static void register(IEventBus eventBus) {
         ENTITY_ENCHANTMENT_EFFECTS.register(eventBus);
--- src/main/java/dev/itsLarss/arcanewilds/enchantment/ModEnchantments.java
+++ src/main/java/dev/itsLarss/arcanewilds/enchantment/ModEnchantments.java
@@ -13,29 +13,6 @@ import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.item.enchantment.Enchantment;
 
 public class ModEnchantments {
-    public static final ResourceKey<Enchantment> DRILL_POWER = ResourceKey.create(Registries.ENCHANTMENT,
-            ResourceLocation.fromNamespaceAndPath(ArcaneWilds.MOD_ID, "drill_power"));
-
-    public static void bootstrap(BootstrapContext<Enchantment> context) {
-        var enchantments = context.lookup(Registries.ENCHANTMENT);
-        var items = context.lookup(Registries.ITEM);
-        var pickaxeTag = items.getOrThrow(ItemTags.PICKAXES);
-
-        // Definition des Enchantments
-        Enchantment.EnchantmentDefinition definition = Enchantment.definition(
-                pickaxeTag,
-                pickaxeTag,
-                5,
-                1,
-                Enchantment.dynamicCost(10, 10),
-                Enchantment.dynamicCost(30, 10),
-                5,
-                EquipmentSlotGroup.MAINHAND
-        );
-
-        register(context, DRILL_POWER, Enchantment.enchantment(definition));
-
-    }
 
     private static void register(BootstrapContext<Enchantment> registry, ResourceKey<Enchantment> key, Enchantment.Builder builder) {
         registry.register(key, builder.build(key.location()));
